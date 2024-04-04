8cc3ebfb17e5c12f6d057ce4110af92a843815ed
poni <poni@habitattt.it>
2024-04-04 11:36:38 +0200

    e

--- script.js
+++ script.js
@@ -78,3 +78,15 @@ function addPic(event) {
 
 document.querySelector(".presentazione").addEventListener("click", addPic)
 document.querySelector(".instruction").addEventListener("click", addPic)
+
+let toggle = false
+document.querySelector("#toggle").addEventListener("click", ()=>{
+    if (toggle){
+        document.querySelector(".burger").style.display = "flex"
+        document.querySelector("#toggle").innerHTML = 'X'
+    } else { 
+        document.querySelector(".burger").style.display = "none"
+        document.querySelector("#toggle").innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0 0 50 50"><path d="M 0 9 L 0 11 L 50 11 L 50 9 Z M 0 24 L 0 26 L 50 26 L 50 24 Z M 0 39 L 0 41 L 50 41 L 50 39 Z"></path></svg>'
+    }
+    toggle = !toggle
+})
