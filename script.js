06fd490a27e4234b632832bf23a96c6a6fa288a8
Federico Poni <poni@macchinone.local>
2024-04-04 15:43:53 +0200

    new

--- script.js
+++ script.js
@@ -53,34 +53,37 @@ function bo(){
     bo()
 
 
-    let opening_pics = ['_DSC7702.JPG','Buoni_a_nulla.JPG','Ido.jpg','Ido2.jpg','Sito.JPG'] 
-
-let i = 0;
-let x;
-let y;
-
-function addPic(event) {
-
-        x = event.clientX;
-        y =  event.clientY;
-        r = Math.floor(Math.random() * (10 - (-10)) +  (-10))
-
-        document.querySelector(".presentazione").innerHTML+=`<img class="opening-pic" style="top:${y}px; left:${x}px; transform: rotate(${r}deg)" src="apertura/${opening_pics[i]}">`
-        i++
-        if (i >= opening_pics.length){
-            i = 0
-        }
-    }
 
 
 
+// DA TENERE
+
+//     let opening_pics = ['_DSC7702.JPG','Buoni_a_nulla.JPG','Ido.jpg','Ido2.jpg','Sito.JPG'] 
+
+// let i = 0;
+// let x;
+// let y;
+
+// function addPic(event) {
+
+//         x = event.clientX;
+//         y =  event.clientY;
+//         r = Math.floor(Math.random() * (10 - (-10)) +  (-10))
+
+//         document.querySelector(".presentazione").innerHTML+=`<img class="opening-pic" style="top:${y}px; left:${x}px; transform: rotate(${r}deg)" src="apertura/${opening_pics[i]}">`
+//         i++
+//         if (i >= opening_pics.length){
+//             i = 0
+//         }
+//     }
+
+// document.querySelector(".presentazione").addEventListener("click", addPic)
 
 
-document.querySelector(".presentazione").addEventListener("click", addPic)
-// document.querySelector(".instruction").addEventListener("click", addPic)
+
 
 function toggleMenu(tog){
-    // if (!window.matchMedia("(max-width: 600px)")){
+    
         if (toggle){
             document.querySelector(".burger").style.display = "flex"
             document.querySelector("#toggle").innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0 0 50 50">        <path d="M 7.71875 6.28125 L 6.28125 7.71875 L 23.5625 25 L 6.28125 42.28125 L 7.71875 43.71875 L 25 26.4375 L 42.28125 43.71875 L 43.71875 42.28125 L 26.4375 25 L 43.71875 7.71875 L 42.28125 6.28125 L 25 23.5625 Z"></path></svg>'
@@ -89,7 +92,6 @@ function toggleMenu(tog){
             document.querySelector("#toggle").innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0 0 50 50"><path d="M 0 9 L 0 11 L 50 11 L 50 9 Z M 0 24 L 0 26 L 50 26 L 50 24 Z M 0 39 L 0 41 L 50 41 L 50 39 Z"></path></svg>'
         }
         toggle = !toggle
-    // }
     
 }
 
@@ -97,3 +99,17 @@ let toggle = true
 document.querySelector("#toggle").addEventListener("click", (toggle)=>{
    toggleMenu()
 })
+
+
+
+var swiper = new Swiper(".swiper", {
+    loop: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+    pagination: {
+      el: ".swiper-pagination",
+      dynamicBullets: true,
+    },
+  });
