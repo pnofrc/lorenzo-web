1ec56e3db488e361e77e86551dbe647820e07eb7
poni <poni@habitattt.it>
2024-04-04 14:12:01 +0200

    e

--- script.js
+++ script.js
@@ -80,14 +80,17 @@ document.querySelector(".presentazione").addEventListener("click", addPic)
 // document.querySelector(".instruction").addEventListener("click", addPic)
 
 function toggleMenu(tog){
-    if (toggle){
-        document.querySelector(".burger").style.display = "flex"
-        document.querySelector("#toggle").innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0 0 50 50">        <path d="M 7.71875 6.28125 L 6.28125 7.71875 L 23.5625 25 L 6.28125 42.28125 L 7.71875 43.71875 L 25 26.4375 L 42.28125 43.71875 L 43.71875 42.28125 L 26.4375 25 L 43.71875 7.71875 L 42.28125 6.28125 L 25 23.5625 Z"></path></svg>'
-    } else { 
-        document.querySelector(".burger").style.display = "none"
-        document.querySelector("#toggle").innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0 0 50 50"><path d="M 0 9 L 0 11 L 50 11 L 50 9 Z M 0 24 L 0 26 L 50 26 L 50 24 Z M 0 39 L 0 41 L 50 41 L 50 39 Z"></path></svg>'
-    }
-    toggle = !toggle
+    // if (!window.matchMedia("(max-width: 600px)")){
+        if (toggle){
+            document.querySelector(".burger").style.display = "flex"
+            document.querySelector("#toggle").innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0 0 50 50">        <path d="M 7.71875 6.28125 L 6.28125 7.71875 L 23.5625 25 L 6.28125 42.28125 L 7.71875 43.71875 L 25 26.4375 L 42.28125 43.71875 L 43.71875 42.28125 L 26.4375 25 L 43.71875 7.71875 L 42.28125 6.28125 L 25 23.5625 Z"></path></svg>'
+        } else { 
+            document.querySelector(".burger").style.display = "none"
+            document.querySelector("#toggle").innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0 0 50 50"><path d="M 0 9 L 0 11 L 50 11 L 50 9 Z M 0 24 L 0 26 L 50 26 L 50 24 Z M 0 39 L 0 41 L 50 41 L 50 39 Z"></path></svg>'
+        }
+        toggle = !toggle
+    // }
+    
 }
 
 let toggle = true
