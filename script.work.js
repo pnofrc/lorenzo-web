951d4d906ef4ed2b1ad0c73700bed644b9be35e7
poni <poni@habitattt.it>
2024-04-26 15:32:04 +0200

    OK

---
+++ script.work.js
@@ -0,0 +1,36 @@
+
+
+let modal = document.querySelector("#modal")
+let closemodal = document.querySelector("#closemodal")
+
+
+function openModal(){
+    // modal.style.display = "block"
+   modal.style.display = 'block'
+   document.querySelector(".corpus").style.overflow = 'hidden'
+}
+
+closemodal.addEventListener("click", ()=>{
+        modal.style.display = 'none'
+
+})
+
+
+
+var swiper = new Swiper(".swiper", {
+    loop: true,
+    clickable: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+
+    //   pagination: {
+    //     el: ".swiper-pagination",
+    //     dynamicBullets: true,
+    //   },
+  });
